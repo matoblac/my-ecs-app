e886d174450daf6e39edb2033ce3ce82531ff228
matoblac <matoblac@amazon.com>
2025-06-08 11:09:33 -0400

    added basic cdk code for ecs architecture

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: cdk.App, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Use default VPC (or create one)
+    const vpc = ec2.Vpc.fromLookup(this, 'DefaultVPC', { isDefault: true });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create ECS cluster
+    const cluster = new ecs.Cluster(this, 'AppCluster', { vpc });
+
+    // Reference existing ECR repo
+    const repository = ecr.Repository.fromRepositoryName(this, 'AppRepo', 'my-ecs-app');
+
+    // Define Fargate service using ALB + latest image
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'FargateService', {
+      cluster,
+      cpu: 512,
+      memoryLimitMiB: 1024,
+      desiredCount: 1,
+      publicLoadBalancer: true,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromEcrRepository(repository, 'latest'),
+        containerPort: 8080, // adjust based on your app
+      },
+    });
   }
 }
