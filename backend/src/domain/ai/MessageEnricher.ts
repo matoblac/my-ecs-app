c12a2e4c37f833abe363f86a92100146230c6f69
matoblac <matoblac@amazon.com>
2025-06-10 17:33:05 -0400

    [FIX] failing tests

--- backend/src/domain/ai/MessageEnricher.ts
+++ backend/src/domain/ai/MessageEnricher.ts
@@ -1,6 +1,8 @@
 import { ChatMessage } from "../chat/ChatMessage";
 import { logInfo } from "../../utils/logger";
 
+
+
 // This is a stub for the MessageEnricher interface. 
 // This MessageEnricher would be launched from the backend and would be used to enrich the user's messages before they are sent to the LLM.
 // It is initiated by LLMClient.chat()
